05799d5cbd8cf5647282bc2a448cc3cdd953c564
Daniel Leaubon <leaubondaniel@gmail.com>
2023-10-04 14:50:16 -0300

    ty

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
 const express = require('express')
 const app = express()
-const fs = require('fs')
+const mongoose = require('mongoose')
 app.use(express.json())
 
-app.get('/get/:email', (req,res)=>{
-    const dados = fs.readFileSync(__dirname+'/'+req.params.email+'.json')
-    res.send(dados)
+const modelodeUsuario = mongoose.model('contas', new mongoose.Schema({
+    email: String,
+    password: String
+}))
+
+mongoose.connect("mongodb://localhost:27017/newdeal")
+.then(function(){
+
+app.get('/get/:email', async (req,res)=>{
+    const usuarioEncontrado = await modelodeUsuario.findOne({email: req.params.email})
+    console.log(usuarioEncontrado);
+    res.send(usuarioEncontrado)
+})
+ 
+app.post('/post',async (req,res) =>{
+    const usuarioCriado = await modelodeUsuario.create({email: req.body.email, password: req.body.password})
+    res.send(usuarioCriado)
 })
 
-app.post('/post', (req,res)=>{
-  fs.writeFileSync(req.body.email+'.json', JSON.stringify(req.body))
-  res.send('Seus dados foram enviados')
+app.put('/put', async (req,res)=>{
+    const usuarioAtualizado = await modelodeUsuario.findOneAndUpdate({email: req.body.email, password: req.body.password}, {email: req.body.newemail, password: req.body.newpassword})
+    res.send(usuarioAtualizado)
 })
-
-app.put('/put', (req,res)=>{
-    fs.writeFileSync(req.body.email+'.json', JSON.stringify(req.body), {flag: 'W'})
-    res.send('Seus dados foram atualizados')
-  })
-app.delete('/delete', (req,res)=>{
-    fs.unlink(__dirname+'/'+req.body.email+'.json')
-    res.send('Seus dados foram deletados')
-  })
+  
+app.delete('/delete', async (req,res)=>{
+    const usuarioDeletado = await modelodeUsuario.deleteOne({email: req.body.email, password: req.body.password})
+    res.send(usuarioDeletado)
+})  
 
 app.use((req,res)=>{
-    res.send('não foi possível encontrar sua rota')
+    res.send('Não foi possível encontrar sua rota')
 })
 
-app.listen(3000, ()=>console.log(`o servidor ta rodando nessa porta aí meu fiel ${5555}`))
+app.listen(3000, ()=>console.log(`O servidor ta rodando nessa porta aí meu fiel ${3000}`))
+
+})
