f4443b19f4c3525708fb04f561d65be43f68d0a2
fooxyta <livya.souza@gsuite.iff.edu.br>
2023-12-06 23:31:53 -0300

    salvou

--- server.js
+++ server.js
@@ -1,53 +1,72 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = 3000;
 app.use(express.json())
 
+mongoose.connect('mongodb://localhost:27017/livya');
 
-const dotenv = require('dotenv')
+const user = mongoose.model('user', {
+  email: String,
+  password: String,
+  name: String,
+});
 
-if (process.env.devouprod === 'dev') {
-dotenv.config({path: './config/.env.dev'})
-}
+const poesia = mongoose.model('poesia', {
+  userId: String,
+  titulo: String,
+  poesia: String,
+  likes: { type: Number, default: 0 },
+});
 
-if (process.env.devouprod === 'prod') {
-    dotenv.config({path: './config/.env.prod'})
-}
-const modelodeUsuario = mongoose.model('contas', new mongoose.Schema({
-    email: String,
-    password: String
-}))
+app.post('/cadastro', async (req, res) => {
+    const dados = await user.create({
+        email: req.body.email,
+        password: req.body.senha,
+        name: req.body.name,
+    })
+    res.json({ message: 'Cadastrou' })
 
-    mongoose.connect('mongodb://127.0.0.1:27017/macaco')
-    .then(function(){
+});
 
-app.post('/get/', async (req,res)=>{
-    const usuarioEncontrado = await modelodeUsuario.findOne({email: req.body.email, password: req.body.password})
-    if (usuarioEncontrado === null) {
-       return res.send('Essa conta não existe')
+app.post('/login', async (req, res) => {
+    const user = await user.findOne({ email: req.body.email });
+    if (!user) {
+        res.json({ message: 'Email ou senha incorretos.' });
+    }else{
+        res.json({ message: 'Entrou' });
     }
-    res.send(usuarioEncontrado)
-})
- 
-app.post('/post',async (req,res) =>{
-    const usuarioCriado = await modelodeUsuario.create({email: req.body.email, password: req.body.password})
-    res.send(usuarioCriado)
-})
 
-app.put('/put', async (req,res)=>{
-    const usuarioAtualizado = await modelodeUsuario.findOneAndUpdate({email: req.body.email, password: req.body.password}, {email: req.body.newemail, password: req.body.newpassword})
-    res.send({message: "Seus dados foram atualizados mano"})
-})
-  
-app.delete('/delete', async (req,res)=>{
-    const usuarioDeletado = await modelodeUsuario.deleteOne({email: req.body.email, password: req.body.password})
-    res.send(usuarioDeletado)
-})  
+});
 
-app.use((req,res)=>{
-    res.send('Não foi possível encontrar sua rota')
-})
+app.post('/deletar', async (req, res) => {
+    const user = await user.deleteOne({ email: req.body.email });
+    res.json({ message: 'Excluido com sucesso' });
 
-app.listen(3000, ()=>console.log(`O servidor ta rodando nessa porta aí meu fiel ${3000}`))
 
-})
+});
+
+app.post('/poesia', async (req, res) => {
+    const dados2 = await user.findOne({ email: req.body.email });
+    const dados = await poesia.create({
+        userId: dados2._id,
+        titulo: req.body.titulo,
+        poesia: req.body.poesia,
+    })
+
+    res.json({ message: 'Poesia Cadastrada com sucesso.' });
+  });
+
+app.post('/like', async (req, res) => {
+    const poetry = await poesia.findById(req.body.poesiaId);
+    if (!poetry) {
+        return res.status(404).json({ message: 'Poesia não encontrada.' });
+    }
+    poetry.likes += 1;
+    await poetry.save();
+    res.json({ message: 'Curtida adicionada!' });
+});
+
+
+app.listen(port);
